cf7eac74f22f0d37fdc8e43d20130d70c2df0a1b
leaf-lxh <meleaf@qq.com>
2021-05-16 21:48:25 +0800

    add MusicInfo customized comparison logic

--- app/src/main/java/com/inkneko/heimusic/entity/MusicInfo.java
+++ app/src/main/java/com/inkneko/heimusic/entity/MusicInfo.java
@@ -3,6 +3,8 @@ package com.inkneko.heimusic.entity;
 import android.graphics.Bitmap;
 import android.net.Uri;
 
+import androidx.annotation.Nullable;
+
 /**
  * 音乐信息的定义。
  */
@@ -27,5 +29,14 @@ public class MusicInfo {
 
     public String getArtistName() {
         return artistName;
+    }
+
+    @Override
+    public boolean equals(@Nullable Object obj) {
+        return (obj != null
+                && this.getClass() == obj.getClass()
+                && this.songName.equals(((MusicInfo) obj).songName)
+                && this.albumName.equals(((MusicInfo) obj).albumName)
+                && this.artistName.equals(((MusicInfo) obj).artistName));
     }
 }
