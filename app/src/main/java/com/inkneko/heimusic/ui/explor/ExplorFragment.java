8888123c51fed0df4bf94b65c3af2d844fa9d18f
leaf-lxh <meleaf@qq.com>
2021-05-16 21:48:52 +0800

    refactored fragment name

--- app/src/main/java/com/inkneko/heimusic/ui/explor/ExplorFragment.java
+++ app/src/main/java/com/inkneko/heimusic/ui/explor/ExplorFragment.java
@@ -1,4 +1,4 @@
-package com.inkneko.heimusic.ui.notifications;
+package com.inkneko.heimusic.ui.explor;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -14,17 +14,17 @@ import androidx.lifecycle.ViewModelProviders;
 
 import com.inkneko.heimusic.R;
 
-public class NotificationsFragment extends Fragment {
+public class ExplorFragment extends Fragment {
 
-    private NotificationsViewModel notificationsViewModel;
+    private ExplorViewModel explorViewModel;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        notificationsViewModel =
-                ViewModelProviders.of(this).get(NotificationsViewModel.class);
-        View root = inflater.inflate(R.layout.fragment_notifications, container, false);
+        explorViewModel =
+                ViewModelProviders.of(this).get(ExplorViewModel.class);
+        View root = inflater.inflate(R.layout.fragment_explor, container, false);
         final TextView textView = root.findViewById(R.id.text_notifications);
-        notificationsViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
+        explorViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
             @Override
             public void onChanged(@Nullable String s) {
                 textView.setText(s);
--- app/src/main/java/com/inkneko/heimusic/ui/explor/ExplorViewModel.java
+++ app/src/main/java/com/inkneko/heimusic/ui/explor/ExplorViewModel.java
@@ -1,16 +1,16 @@
-package com.inkneko.heimusic.ui.dashboard;
+package com.inkneko.heimusic.ui.explor;
 
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
-public class DashboardViewModel extends ViewModel {
+public class ExplorViewModel extends ViewModel {
 
     private MutableLiveData<String> mText;
 
-    public DashboardViewModel() {
+    public ExplorViewModel() {
         mText = new MutableLiveData<>();
-        mText.setValue("This is dashboard fragment");
+        mText.setValue("This is explor fragment");
     }
 
     public LiveData<String> getText() {
